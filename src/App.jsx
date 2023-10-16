935d5783ab5b2c2c9689d036dda7d7ec8d77f863
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-10-16 18:56:27 +0300

    exam-5

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ function Hi({name}) {
   return <h1> Hi, {name}!</h1>
 }
 
+function Sum({a, b}) {
+  const sum = a + b
+  return <h2>Sum: {sum}</h2>
+}
+
 function App() {
   
   return (
     <>
-      <Hi name/>
+      <Hi name="Emre"/>
+      <Sum a={10} b={7}/>
     </>
   )
 }
