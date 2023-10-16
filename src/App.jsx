3843bb8980f8e301b88884829a2b8b997e32b6c0
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-10-16 18:22:43 +0300

    exam-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
+function Hi({name}) {
+  return <h1> Hi, {name}!</h1>
+}
+
 function App() {
-  const hello = "Helo World!"
+  
   return (
     <>
-      <h1>{hello}</h1>
+      <Hi name="Mario"/>
     </>
   )
 }
