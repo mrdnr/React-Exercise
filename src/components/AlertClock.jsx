77c4fe0b2b319beb9427466a11f0af90363b40fa
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-10-19 12:12:00 +0300

    exam-27

--- src/components/AlertClock.jsx
+++ src/components/AlertClock.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-function AlertClock() {
-  const getCurrentTime = () => {
-    const currentTime = new Date();
-    alert("Time : " + currentTime.toLocaleTimeString());
-  };
+function AlertClock({onClick}) {
+  
 
   return (
     <div>
-      <button onClick={getCurrentTime}>Show Current Time</button>
+      <button onClick={onClick}>Show Current Time</button>
     </div>
   );
 }
