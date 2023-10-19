2f0de7146a3c27e23ae7095cc0c3ffa481154029
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-10-19 13:24:29 +0300

    exa-30

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Counter() {
     const [count, setCount] = useState(0);
-
+    //An instantaneous value is enough because react updates piece by piece due to its structure. Each time the button is clicked, an update takes place.
     return ( 
         <div>
             <h2>Count: {count}</h2>
