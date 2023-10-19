7bfc8aa56b73400bf48cd0afd44eff9c78688d81
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-10-19 20:51:47 +0300

    exam-35

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,22 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function Display({count}){
-  return <h2>Counter :{count}</h2>
-}
+function Clock() {
+  const [time, setDate] = useState(new Date());
 
-function Counter({ value, amount }) {
-  const [count, setCount] = useState(value);
+  useEffect(() => {
+    setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+  }, [time]);
 
-  const handleIncrement = () => {
-    setCount(count + amount);
-  };
-  const handleDecrement = () => {
-    setCount(count - amount);
-  };
-  const handleReset = () => {
-    setCount(value);
-  };
-  useEffect(()=>{
-    console.log("Current value :", count)
-  },[count])
-
-
-  return (
-    <div>
-      <Display count = {count}/>
-      <button onClick={handleIncrement}>Increase</button>
-      <button onClick={handleDecrement}>Decrease</button>
-      <button onClick={handleReset}>Reset</button>
-    </div>
-  );
+  return <h2>{time.toLocaleString()}</h2>;
 }
 
 const App = () => {
   return (
     <div className="main">
-      <Counter value={0} amount={2} />
+      <Clock/>
     </div>
   );
 };
