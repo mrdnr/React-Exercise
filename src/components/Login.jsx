93cb0f3efa05ad9206990f05f283d9529f604c8a
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-10-23 14:38:53 +0300

    exam-43

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,62 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [remember, setRemember] = useState(false);
-  
-    const handleUsername = (e) => {
-      setUsername(e.target.value);
-    };
-  
-    const handlePassword = (e) => {
-      setPassword(e.target.value);
-    };
-  
-    const handleRemember = (e) => {
-      setRemember(e.target.checked);
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-    };
-  
+function Login({ handleLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
 
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRemember = (e) => {
+    setRemember(e.target.checked);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleLogin({ username, password, remember });
+  };
+
+  const isForm = username.trim() !== "" && password.trim() !== "";
   return (
     <div>
-        <form onSubmit={handleSubmit}>
-      <label htmlFor="username">
-        <input  type="text" placeholder="User Name" id="username" value={username} onChange={handleUsername}/>
-      </label>
-      <label htmlFor="Password">
-        <input type="text" placeholder="Password" id="password" value={password} onChange={handlePassword}/>
-      </label>
-      <label htmlFor="remember "> Don't forget me
-        <input type="checkbox" id="remember" checked={remember} onChange={handleRemember}/>
-      </label>
-      <button type="submit">Login</button>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          <input
+            type="text"
+            placeholder="User Name"
+            id="username"
+            value={username}
+            onChange={handleUsername}
+          />
+        </label>
+        <label htmlFor="Password">
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={handlePassword}
+          />
+        </label>
+        <label htmlFor="remember ">
+          {" "}
+          Don't forget me
+          <input
+            type="checkbox"
+            id="remember"
+            checked={remember}
+            onChange={handleRemember}
+          />
+        </label>
+        <button type="submit" disabled={!isForm}>
+          Login
+        </button>
       </form>
     </div>
   );
