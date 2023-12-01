fc3c0502e125288c6b1d613dacd2ef9960094c04
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-12-01 15:12:10 +0300

    Uncontrolled Forms 3

--- src/components/UncontrolledLogin .jsx
+++ src/components/UncontrolledLogin .jsx
@@ -1,79 +1,92 @@
 import { useState } from "react";
+import "./App.css";
 
 function Login() {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-    remember: "",
-  }); // Assuming you have state variables for username and password
+	const [data, setData] = useState({
+		username: "",
+		password: "",
+		remember: "",
+	});
 
-  function handleSubmit(e) {
-    e.preventDefault();
+	function handleSubmit(e) {
+		e.preventDefault();
 
-    console.log(formData);
-    setFormData({
-      username: "",
-      password: "",
-      remember: "",
-    });
-  }
+		// The built in javascript object which is FormData should be used in this case. 
+		const formData = new FormData(e.target)
 
-  function handleReset() {
-    setFormData({
-      username: "",
-      password: "",
-      remember: "",
-    });
-  }
+		console.log(formData.get('username'));
+		setData({
+			username: "",
+			password: "",
+			remember: "",
+		});
+	}
 
-  function handleChange(e) {
-    const { name, value, type, checked } = e.target;
-    console.log(name);
+	function handleReset() {
+		setData({
+			username: "",
+			password: "",
+			remember: "",
+		});
+	}
 
-    setFormData({
-      ...formData,
-      [name]: type === "checkbox" ? checked : value,
-    });
-  } // With handleChange event we can access username and password using setFormData.
+	function handleChange(e) {
+		const { name, value, type, checked } = e.target;
+		console.log(name);
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="remember">Don't Forget Me:</label>
-        <input
-          type="checkbox"
-          name="remember"
-          checked={formData.remember}
-          onChange={handleChange}
-        />
-      </div>
-      <button
-        disabled={formData.username === "" || formData.password === ""}
-        type="submit"
-      >
-        Submit
-      </button>
-      <button onClick={handleReset}>Reset</button>
-    </form>
-  );
+		setData({
+			...data,
+			[name]: type === "checkbox" ? checked : value,
+		});
+	}
+
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<div>
+				<label htmlFor="username">Username:</label>
+				<input
+					type="text"
+					name="username"
+					value={data.username}
+					onChange={handleChange}
+				/>
+			</div>
+			<div>
+				<label htmlFor="password">Password:</label>
+				<input
+					type="password"
+					name="password"
+					value={data.password}
+					onChange={handleChange}
+				/>
+			</div>
+			<div>
+				<label htmlFor="remember">Dont Forget Me:</label>
+				<input
+					type="checkbox"
+					name="remember"
+					checked={data.remember}
+					onChange={handleChange}
+				/>
+			</div>
+			<button
+				disabled={data.username === "" || data.password === ""}
+				type="submit"
+			>
+				Submit
+			</button>
+			<button onClick={handleReset}>Reset</button>
+		</form>
+	);
 }
 
-export default Login;
+const App = () => {
+	return (
+		<div id="app">
+			<Login />
+		</div>
+	);
+};
+
+export default App;
