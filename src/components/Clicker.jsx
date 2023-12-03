e9898452a5a9cda345530720a39ca3995b63022f
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-12-03 16:10:02 +0300

    39 handling event fix

--- src/components/Clicker.jsx
+++ src/components/Clicker.jsx
@@ -1,19 +1,22 @@
-
-
 function MouseClicker() {
-
-    function handleClicker(e){
-        return console.log(e.target.name);
-    }
-
-    return (
-		<>
-			<button name="one" onClick={handleClicker}>Click Me
-                <img src="https://www.view-page-source.com/i/view-source-cloud.webp" width={50} height={50} onClick={(e) => console.log(e.target.src)}/>
-            </button>
-            {/* Only change CurrentTarget to target */}
-		</>
-	);
+  function handleClicker(e) {
+    console.log(e.target.src);
+  }
+  
+  return (
+    <>
+      <button name="one"> 
+        Click Me
+        <img
+          src="https://www.view-page-source.com/i/view-source-cloud.webp"
+          width={50}
+          height={50}
+          onClick={handleClicker}
+        />
+      </button>
+      
+    </>
+  );
 }
 
 export default MouseClicker;
