f5ab075b7d39e41eab9e33c4826ca9b6bb1a5fc5
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-12-03 21:42:46 +0300

    69 Component Composition

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ const App = () => {
     <div>
       <Container title="My Container Title">
         <p>This content is inside the custom container.</p>
-        {/* Add more child components/content as needed */}
       </Container>
     </div>
   );
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,19 @@
 import './Container.css';
+import { useState } from 'react';
 
 const Container = ({ title, children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
-    <div className="custom-container">
-      {title && <h2 className="container-title">{title}</h2>}
-      {children}
+    <div className={`custom-container ${collapsed ? 'collapsed' : ''}`}>
+      <h2 className="container-title" onClick={toggleCollapse}>
+        {title}
+      </h2>
+      {!collapsed && <div className="container-content">{children}</div>}
     </div>
   );
 };
